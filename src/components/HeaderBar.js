4ba35472872ee97908e067e98ade7eb493a5c6d2
Chris <cjcorbin@gmail.com>
2020-09-25 09:47:59 -0700

    Minor Styling

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -26,7 +26,7 @@ const FeedBar = () => {
     return (
         <div className="howto-header">
             <HeaderStyle>
-            <h1>Welcome To LifeHacker!</h1>
+            <h1>Welcome To Life Hacker!</h1>
                 <img alt="logo" class="center" src="https://cdn.pixabay.com/photo/2017/03/31/23/11/robot-2192617_960_720.png" />
             </HeaderStyle>
         </div>
